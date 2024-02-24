297a4861a32eb5a8add8a6532fe77dc394c07c9d
Tim Finley <tfinley@pop-os.localdomain>
2024-02-24 10:09:47 -0500

    removed sund on success from ttwn

--- src/radautopy/runners/ttwn.py
+++ src/radautopy/runners/ttwn.py
@@ -1,15 +1,21 @@
+import logging
 
 from ..utils.audio import AudioFile
 from ..utils.config import ROOT_DIR
 from ..utils.utilities import handle_status_code
 
+from . import LOGGER_NAME
+
+logger = logging.getLogger(LOGGER_NAME)
+
 def perform_ttwn(config, mailer, email_bool, remote):
-    #affiliate_dir = config.dirs['audio_tmp'] #make_dir(Path(ROOT_DIR, config.ttwn['affiliate']))
     timestamp = remote.probe_timestamp()
     if handle_status_code(remote.manifest.status_code, mailer):
         try:
             url = remote.get_manifest(timestamp)
+            logger.info('got manifest')
         except:
+            logger.critical('could not get manifest')
             mailer.p('Could not get manifest')
             mailer.send_mail(alt_subject = 'An error occurred')
 
@@ -21,9 +27,7 @@ def perform_ttwn(config, mailer, email_bool, remote):
             local_track = AudioFile(track['input_file'], track['output_file'])
             local_track.apply_metadata(track['artist'], track['title'])
             local_track.move()
-            mailer.p(f'Download successful {url = } {track["input_file"] = }')
-            mailer.p(f'Moved {track["input_file"] = } to {track["output_file"] = }')
-            mailer.send_mail()
+            logger.info('successfully moved audio')
         else:
             mailer.p('Something went wrong')
             mailer.send_mail()
